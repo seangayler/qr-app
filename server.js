ad4578df75d7596f80ff6c36a255b59da7d969d4
Sean Gayler <seangayler@Seans-MacBook-Pro.local>
2020-02-28 16:52:20 +1100

    Setup basic server functionality.

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
+// Modules
 const express = require('express');
-const app = express();
-
+const path = require('path');
+const uuid = require('uuid/v4');
+const session = require('express-session');
 const pg = require('pg');
+
+// Routers
+const indexRouter = require('./routes/indexRouter.js');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(session({
+  genid: (req) => {
+    console.log(req.sessionID);
+    return uuid();
+  },
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use('/', indexRouter);
+app.use('/test', (req, res) => {
+  res.send(uuid());
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('\n--- Server started. :) ---\n')
+})
