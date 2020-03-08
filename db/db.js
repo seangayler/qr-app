6afffebdd059e8fd60de5f644889e1c69d29c60e
Sean Gayler <seangayler@Seans-MacBook-Pro.local>
2020-03-08 15:34:44 +1100

    Revert to safe commit.

---
+++ db/db.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    pg.connect((err, pool, ))
+    
+    return pool.query(text, params, callback);
+  }
+}
